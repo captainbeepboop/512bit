919eb63d9ac9cd9948e565d9598dc45acab328a1
captainbeepboop <dr.cris.edwards@gmail.com>
2025-06-08 12:54:56 +1000

    updated upload

--- upload.py
+++ upload.py
@@ -4,9 +4,8 @@ file = open('/home/cris-edwards/Desktop/history.txt', 'r')
 
 new_title = file.readline()
 new_title = new_title.replace(":", "").replace(" ", "_").replace("/", "_").strip()+".txt"
-command = f'cp history.txt ~/Desktop/github/{new_title}'
+command = f'cp history.txt {new_title}'
 os.system(command)
-os.chdir("/home/cris-edwards/Desktop/github")
 command = f'git add {new_title}'
 os.system(command)
 os.system('git commit -m "comment"')
