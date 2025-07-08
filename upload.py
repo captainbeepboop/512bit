0dcb895a52ddef511a1060f341bb427c01a18478
Captain Beepboop <119025473+captainbeepboop@users.noreply.github.com>
2025-07-08 16:11:12 +1000

    One big history

--- upload.py
+++ upload.py
@@ -1,13 +1,18 @@
 import os
 
-file = open('/home/cris-edwards/Desktop/history.txt', 'r')
+#file = open('/home/cris-edwards/Desktop/history.txt', 'r')
 
-new_title = file.readline()
-new_title = new_title.replace(":", "").replace(" ", "_").replace("/", "_").strip()+".txt"
-command = f'cp history.txt {new_title}'
-os.system(command)
-command = f'git add {new_title}'
-os.system(command)
-os.system('git commit -m "comment"')
+#new_title = file.readline()
+#new_title = new_title.replace(":", "").replace(" ", "_").replace("/", "_").strip()+".txt"
+#command = f'cp history.txt {new_title}'
+#os.system(command)
+#command = f'git add {new_title}'
+#os.system(command)
+#os.system('git commit -m "comment"')
+#os.system('git push')
+#os.remove(new_title)
+
+os.system('git pull')
+os.system('git add history.txt')
+os.system('git commit -m "update history"')
 os.system('git push')
-os.remove(new_title)
